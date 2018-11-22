9df9c5ce5a1009d884da93ccedece375dafa084c
Talita Gregory <talitagregory@hotmai.com>
2018-11-22 21:36:50 -0200

    codes restructure

---
+++ src/components/Layout/Sidebar/index.js
@@ -0,0 +1,57 @@
+import React  from 'react';
+
+import Avatar       from '@material-ui/core/Avatar';
+import Divider      from '@material-ui/core/Divider';
+import Drawer       from '@material-ui/core/Drawer';
+import List         from '@material-ui/core/List';
+import ListItem     from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography   from '@material-ui/core/Typography';
+
+import './Sidebar.css';
+
+
+const TemporaryDrawer = (props) => {
+
+    return (
+      <Drawer 
+        open={props.collapsed} 
+        onClose={props.collapseSidebar}
+      >
+        <div className="Profile">
+          <Avatar
+            alt={props.profileName}
+            src={props.profilePicture}
+            className="Profile-picture"
+          />
+          
+          <Typography 
+            variant="title"
+            className="Profile-name"
+          >
+            {props.profileName}
+          </Typography>
+
+          <Typography 
+            variant="body1"
+            className="Profile-email"
+          >
+            {props.profileEmail}
+          </Typography>
+
+        </div>
+
+        <Divider />
+        <List>
+          {props.menu.map((text, index) => (
+            <ListItem button key={index}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      
+  );
+}
+
+export default TemporaryDrawer;
---
+++ src/layout/Dashboard.js
@@ -0,0 +1,50 @@
+import React  from 'react';
+import Sidebar from '../components/Layout/Sidebar';
+import Header from '../components/Layout/Header'  ;
+
+class Dashboard extends React.Component{
+    state = {
+        sidebar:{
+            collapsed: false,
+            items : ['Página inicial', 'Banca', 'TCC']
+        },
+        user: {
+            notification: 70,
+            picture: 'https://pmcdeadline2.files.wordpress.com/2018/09/trump1.jpg?w=446&h=299&crop=1',
+            name: 'Donaldzinho',
+            email: 'aloca@usa.com'
+        },
+        calendar:{
+
+        }
+    }
+
+    toggleDrawer = () => {
+        this.setState({
+            sidebar: {
+                collapsed: !this.state.sidebar.collapsed,
+                items: this.state.sidebar.items
+            }
+        });
+    };
+    render(){
+        return(
+            <div>
+                <Header  
+                    collapseSidebar={this.toggleDrawer} 
+                    notification={this.state.user.notification}
+                />
+                <Sidebar 
+                    collapsed={this.state.sidebar.collapsed} 
+                    collapseSidebar={this.toggleDrawer}
+                    profilePicture={this.state.user.picture} 
+                    profileName={this.state.user.name}
+                    profileEmail={this.state.user.email}
+                    menu={this.state.sidebar.items}
+                />
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
