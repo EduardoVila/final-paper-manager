cd5f86f9726f25c994c8704a003e37d3498e0042
EduardoVila <eduardodevila1@hotmail.com>
2018-12-11 16:18:56 -0200

    Banca View

---
+++ src/components/DadosBanca/index.js
@@ -0,0 +1,94 @@
+import React from "react"
+
+import Button          from '@material-ui/core/Button'
+import FormGroup       from '@material-ui/core/FormGroup'
+import Paper           from '@material-ui/core/Paper'
+import TextField       from '@material-ui/core/TextField'
+import Typograph       from '@material-ui/core/Typography'
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MultipleSelectName from '../MultipleSelectName';
+import './DadosBanca.css'
+
+const DadosBanca = (props) => (
+
+  <Paper
+    elevation={9}
+  >
+    <form 
+      className='DadosBanca'
+      autoComplete='off' 
+    >
+
+      <FormGroup row>
+        <Typograph className="title"
+          variant='display1'
+        >
+          Cadastro da Banca
+        </Typograph>
+      </FormGroup>  
+
+      <TextField
+        label='Título do TCC'
+        margin='normal'
+        fullWidth={true}
+        required={true}
+      />
+        <FormControl required className='FinalPaperForm-control'>
+          <InputLabel htmlFor="orientador-required">
+            Local 
+          </InputLabel>
+          <Select
+            name="age"
+            inputProps={{
+              id: 'orientador-required',
+            }}
+          >
+            <MenuItem value={30}>UFSC - Jardim das Avenidas</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl required className='FinalPaperForm-control'>
+          <InputLabel htmlFor="orientador-required">
+            Sala 
+          </InputLabel>
+          <Select
+            name="age"
+            inputProps={{
+              id: 'orientador-required',
+            }}
+          >
+            <MenuItem value={30}>302</MenuItem>
+          </Select>
+        </FormControl>
+      <FormGroup row className='DadosBanca-twoColumns'>
+        <TextField
+        required={true}
+        id="date"
+        label="Data da Banca"
+        type="date"
+        defaultValue="2017-05-24"
+        className={'bancaDate'}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      </FormGroup>
+        <MultipleSelectName/>
+      <FormGroup row className='DadosBanca-twoColumns'>
+        <Button 
+          variant="contained" 
+          color="primary" 
+          onClick={props.plusButton}
+        >
+        Enviar
+        </Button> 
+
+      </FormGroup>
+    </form>
+  </Paper>
+
+)
+
+export default DadosBanca;
--- src/views/ExaminationBoard.js
+++ src/views/ExaminationBoard.js
@@ -3,16 +3,38 @@ import React, {Component} from 'react'
 import Dashboard from '../layout/Dashboard'
 import Grid      from '@material-ui/core/Grid'
 import Paper     from '@material-ui/core/Paper'
+import ConfirmChip from '../components/ConfirmChip'
+import Typograph       from '@material-ui/core/Typography'
+import DadosBanca from '../components/DadosBanca'
 
 class ExaminationBoard extends Component {
+
+    state = {
+        condicao: true,
+        name: 'Eduardo de Vila'
+    };
     render() {
         return (
             <Dashboard>
-                <Grid item xs={12} md={6}>
-                    <Paper>Examination Board's form</Paper>
+                <Grid item xs={12} md={8}>
+                    <DadosBanca/>
                 </Grid>
-                <Grid item xs={12} md={6}>
-                    <Paper>Presentation Schedule</Paper>
+                <Grid item xs={12} md={4}>
+                <Paper>
+                    <Typograph variant='display1' className="title title1">
+                        Confirmação Integrantes
+                    </Typograph>
+                    <div className="integrantesChip">
+                        <ConfirmChip {...{condicao: true, name: 'Eduardo de Vila'}}></ConfirmChip>
+                        <ConfirmChip {...{condicao: false, name: 'Joao Manoel'}}></ConfirmChip>
+                        <ConfirmChip {...{condicao: true, name: 'Olivia Palito'}}></ConfirmChip>
+                        <ConfirmChip {...{condicao: false, name: 'Silvio Santos'}}></ConfirmChip>
+                    </div>
+
+                </Paper>
+                    
+                       
+                   
                 </Grid>
             </Dashboard>
         );
